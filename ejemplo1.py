ae81d39b0b44df388d2d6d8c41bd62a0c98c772e
vidalone <vidalone@hotmail.com>
2026-06-05 09:46:55 +0000

    Primer cambio

---
+++ ejemplo1.py
@@ -0,0 +1,39 @@
+import keyword
+#import os
+#import pandas as pd
+
+# Comentarios
+
+# Comentarios a añadir al agente de IA para que me ayude a lo largo del curso de Python básico de Fortinux
+"""
+Estoy realizando el curso de python básico de Fortinux. Enseña a usar Python para crear scripts y programas simples plenamente funcionales, desarrollar aplicaciones web, realizar análisis de datos, y utilizar bibliotecas de inteligencia artificial (AI) para aprendizaje automático (ML) a nivel básico. 
+
+Quiero que actúes como mi asistente personal y tutor a lo largo de este curso. 
+
+No me des la respuesta haciendo el trabajo por mí. tu trabajo es: 
+- Ayudarme a entender los conceptos en profundidad. 
+- Explicar el código de manera sencilla. 
+- Ayudarme a aplicar lo que he aprendido en pequeños proyectos. 
+- Ayudarme a entender y resolver errores de código (debugging) y a mejorarlo (refactoring).
+"""
+
+variable = 10
+
+varible2 = 20 # Comentario
+
+def funcion ():
+    x = 8
+    print(x)
+
+# print (variable)
+
+# funcion()
+
+# print(keyword.kwlist) # Muestra las palabras clave que no se pueden usar en Python
+
+print('¡Hola Mundo!')
+
+print("¡Hola Mundo!")
+
+print("Esto es todo", end=" ")
+print("amigos!")
